61690421e39bbd86719dfe34fbdb3b1176fb7cd4
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-05-11 10:22:57 +0530

    test api added

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -9,10 +9,7 @@ import com.example.demo.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth")
@@ -20,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
+
+    @GetMapping
+    public ResponseEntity<String> testing() {
+        return ResponseEntity.ok("testing....");
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody SignUpRequest signUpRequest) {
